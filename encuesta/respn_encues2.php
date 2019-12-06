18707b4ffbe4d3cd92ef22d3fc821e126223c88f
Daniel Heresmann <dheresmann2012@alu.uct.cl>
2019-12-06 20:11:52 -0300

    correcciones

--- encuesta/respn_encues2.php
+++ encuesta/respn_encues2.php
@@ -1118,10 +1118,6 @@ history.pushState(null, null, location.href);
                                 <img style="margin-right: 5px;" width="44" src="../assets/img/mineduc.png">
                                 <img style="margin-right: 5px;" width="120" src="../assets/img/fondef.png">
                                 <img style="margin-right: 5px;" width="100" src="../assets/img/corfo.png">
-                            </div>
-                        </td>
-                        <td align="center" width="33%">
-                            <div>
                                 <img style="margin-right: 5px;" width="30" src="../assets/img/ufro.png">
                                 <img style="margin-right: 5px;" width="90" src="../assets/img/autonoma.png">
                                 <img style="margin-right: 5px;" width="150" src="../assets/img/fund_telefonica.png">
